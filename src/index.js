a7dc2ae1e8de1c7c13cbc2e70d491a005f3349c9
TranDangKhoa <trandangkhoa.automation@gmail.com>
2026-07-10 07:21:24 +0700

    Support Render default start command

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+import { pathToFileURL } from "node:url";
+
 export * from "./common/index.js";
 export * from "./cards/index.js";
+
+// Keep the package exports side-effect free when imported, while also supporting
+// managed hosts that use `node src/index.js` as their default start command.
+if (
+  process.argv[1] &&
+  import.meta.url === pathToFileURL(process.argv[1]).href
+) {
+  await import("../express.js");
+}
